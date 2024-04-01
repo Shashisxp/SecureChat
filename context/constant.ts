2ee97ba0ad489ed2260b3ce2dd4c896c6c8dd2ed
Ayush-kathayat <kathayatayush789@gmail.com>
2024-04-02 02:34:40 +0530

    fix/prod : changed the location of chatapp JSON

--- context/constant.ts
+++ context/constant.ts
@@ -1,5 +1,5 @@
 // 0x5FbDB2315678afecb367f032d93F642f64180aa3
-import chatAppJSON from "..//artifacts/contracts/chatApp.sol/ChatApp.json";
+import chatAppJSON from "./ChatApp.json";
 
 export const ChatAppAddress = "0x0f3E3C102b618F7Ada9c26f2a63E047398115ca6";
 export const ChatAppAbi = chatAppJSON.abi;
